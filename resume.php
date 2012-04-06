03c3f7d4c16cbc1ac36180306e2363fb2311f088
Nick Cox <nickcox1008@gmail.com>
2012-04-06 13:48:52 -0600

    Made nice stylings on the student upload resume page.

--- resume.php
+++ resume.php
@@ -1,5 +1,4 @@
 <?php
-include_once("inc/connect.php");
 include_once("inc/header.php");
 
 // If the session isn't set by logging in or registering, re-route to login.php
@@ -22,108 +21,98 @@ $sql = "SELECT *  FROM students WHERE email='$email'";
 $result = mysql_query($sql);
 $info = mysql_fetch_array($result);
 $resume = $info['resume'];
-if ( !($resume == "NULL" || $resume == '') )
+if ( !($resume == "NULL" || $resume == '') ) {
 	$resume_link = "<a href='resumes/$resume'>Current resume</a>";
-
-if(isset($_GET['error'])) 
-	{
-		$printout = '';
-		
-		// Errors array
-		$errors = array(
-			UPLOAD_ERR_OK			=> "No errors.", 
-			UPLOAD_ERR_INI_SIZE		=> "File is too large.",
-			UPLOAD_ERR_FORM_SIZE    => "File is too large.",
-			UPLOAD_ERR_PARTIAL		=> "Partial upload.",	
-			UPLOAD_ERR_NO_FILE		=> "No file uploaded.",
-			UPLOAD_ERR_NO_TMP_DIR	=> "No temporary directory to upload to.",
-			UPLOAD_ERR_CANT_WRITE	=> "Can't write to disk.",
-			UPLOAD_ERR_EXTENSION	=> "Invalid file type.",
-		);
-		
-		if(is_numeric($_GET['error']))
-			$printout = $errors[ $_GET['error'] ];
-		
-		else
-		{
-			if($_GET['error'] == 'extension')
-				$printout = 'Invalid file type. PDF only.';
-			
-			else if($_GET['error'] == 'size')
-				$printout = 'File is too large. Max file size is 5MB';
-		}
-		
-		echo '<center><div class="error">A server error occured: '. $printout . '</div></center><br />';
+}
+if(isset($_GET['error'])) {
+	$printout = '';
+	
+	// Errors array
+	$errors = array(
+		UPLOAD_ERR_OK			=> "No errors.", 
+		UPLOAD_ERR_INI_SIZE		=> "File is too large.",
+		UPLOAD_ERR_FORM_SIZE    => "File is too large.",
+		UPLOAD_ERR_PARTIAL		=> "Partial upload.",	
+		UPLOAD_ERR_NO_FILE		=> "No file uploaded.",
+		UPLOAD_ERR_NO_TMP_DIR	=> "No temporary directory to upload to.",
+		UPLOAD_ERR_CANT_WRITE	=> "Can't write to disk.",
+		UPLOAD_ERR_EXTENSION	=> "Invalid file type.",
+	);
+	
+	if(is_numeric($_GET['error'])) {
+		$printout = $errors[ $_GET['error'] ];
 	}
+	else {
+		if($_GET['error'] == 'extension')
+			$printout = 'Invalid file type. PDF only.';
+		
+		else if($_GET['error'] == 'size')
+			$printout = 'File is too large. Max file size is 5MB';
+	}
+	
+	echo '<center><div class="error">A server error occured: '. $printout . '</div></center><br />';
+}
 ?>
 <form method="post" name="upload" action="php/resume_script.php" enctype="multipart/form-data">
-<table id="resume_table" class="text">
+<table id="resume_table">
 	<tr>
-		<td>Now, take some time to upload a resume and fill out some basic information in the fields below. While nothing on this page is required, more information can improve your chances for employment. Or <a href="profile.php">continue to profile</a></td>
+		<td colspan=2><br /><h1>Now, take some time to upload a resume and fill out some basic information.</h1><br /><h3>While nothing on this page is required, more information can improve your chances for employment.</h3><br />-OR-<br /><a href="profile.php">Continue to Profile.</a></td>
 	</tr>
 	<tr>
-		<td><br /><p><b>Upload your resume: (.pdf only)</b></p></td>
+		<td colspan=2><hr /></td>
 	</tr>
 	<tr>
-		<td><?php echo $resume_link; ?></td>
+		<td style="width:50%;"><b>Upload your profile picture: (.jpg, .gif, .png only)</b>
+		<td style="width:50%;"><b>Upload your resume: (.pdf only)</b></td>
 	</tr>
 	<tr>
-		<td><br /><input type="file" name="resume" /></td>
+		<td>It will only be displayed as 100x100, so don't<br />waste our space by uploading anything bigger.</td>
+		<td></td>
 	</tr>
 	<tr>
-		<td><hr /></td>
-	</tr>
-	<tr>
-		<td><br /><p><b>Upload your profile picture: (.jpg, .gif, .png only)</b></p></td>
-	</tr>
-	<tr>
-		<td><p>It will only be displayed as 100x100 so don't waste our space by uploading anything bigger.</p></td>
-	</tr>
-	<tr>
-		<td>
-			<br />
-			<input type="file" name="image" />
+		<td><br /><input type="file" name="image" /></td>
+		<td><br /><?php echo $resume_link; ?><br /><input type="file" name="resume" /><br />
 		</td>
 	</tr>
 	<tr>
-		<td><br /><br /><p><b>Fill out your information below:</b></p></td>
+		<td colspan=2><hr /></td>
 	</tr>
 	<tr>
-		<td><hr /></td>
+		<td colspan=2><b>Employment history:</b></td>
 	</tr>
 	<tr>
-		<td>Brief description of yourself:</td>
+		<td><br />
+			<label for="phone">Contact Phone Number: </label>
+			<input type="text" name="phone" maxlength="10" size="10" value="<?php echo $info['contact_phone']; ?>"/> (10 digits)</td>
+		<td><br />
+			<label for="contact_email">Contact Email: </label>
+			<input type="text" name="contact_email" size="40" value="<?php echo $info['contact_email']; ?>" /></td>
 	</tr>
 	<tr>
-		<td><textarea rows="5" cols="50" name="description" ><?php echo $info['description']; ?></textarea></td>
+		<td colspan=2 style="text-align: left; padding-left: 27%;"><br />
+			<input type="checkbox" id="hide_contact" /> Hide my contact information from potential employers?</td>
 	</tr>
 	<tr>
-		<td><br />Brief list of employable qualities:</td>
+		<td colspan=2 style="text-align: left; padding-left: 27%;">
+			<input type="checkbox" name="notification" <?php if($info['notification']){ echo "checked"; } ?>/> Receive new job notifications via email?</td>
 	</tr>
 	<tr>
-		<td><textarea rows="10" cols="50" name="qualities"><?php echo $info['qualities']; ?></textarea></td>
+		<td style="text-align: right;"></td>
+		<td style="text-align: left;"></td>
 	</tr>
 	<tr>
-		<td><br />Employment history:</td>
+		<td colspan=2><br /><hr /></td>
 	</tr>
 	<tr>
-		<td>Hide contact information from potential employers? <input type="checkbox" id="hide_contact" /></td>
+		<td><b>Brief description of yourself:</b></td>
+		<td><b>Brief list of employable qualities:</b></td>
 	</tr>
 	<tr>
-		<td>
-		<br />
-		<div id="contact_info">
-			<label for="phone">Contact Phone Number: </label><input type="text" name="phone" maxlength="10" size="10" value="<?php echo $info['contact_phone']; ?>"/> (10 digits)
-			<br /><br />
-			<label for="contact_email">Contact Email: </label><input type="text" name="contact_email" size="40" value="<?php echo $info['contact_email']; ?>" />
-		</div>
-		</td>
+		<td><br /><textarea rows="10" cols="50" name="description" ><?php echo $info['description']; ?></textarea></td>
+		<td><br /><textarea rows="10" cols="50" name="qualities"><?php echo $info['qualities']; ?></textarea></td>
 	</tr>
 	<tr>
-		<td><br />Receive new job notifications via email? <input type="checkbox" name="notification" <?php if($info['notification']){ echo "checked"; } ?>/></td>	
-	</tr>
-	<tr>
-		<td><br /><button class="button" id="resume_submit">Submit Info</button></td>
+		<td colspan=2><br /><br /><button class="button" id="resume_submit">Submit Info</button></td>
 	</tr>
 </table>
 </form>
